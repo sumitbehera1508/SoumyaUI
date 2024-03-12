757ec010e47154f49925a48373bbf28c51b3c359
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-03-12 16:02:59 +0530

    minor changes

--- app/src/main/java/apps/sumit/temp/MainActivity.kt
+++ app/src/main/java/apps/sumit/temp/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import apps.sumit.temp.presentation.form.InputDetailsScreen
 import apps.sumit.temp.ui.theme.TempTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,29 +19,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             TempTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                Show()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Show() {
+    InputDetailsScreen()
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TempTheme {
-        Greeting("Android")
+        Show()
     }
 }
