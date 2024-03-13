7634c7558a5b9730e4b41d1f9b66b444ab97ba1c
sumitbehera1508 <100491275+sumitbehera1508@users.noreply.github.com>
2024-03-14 01:03:21 +0530

    minor changes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    // id("com.google.gms.google-services")
 }
 
 android {
@@ -66,6 +67,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    //  implementation("com.google.firebase:firebase-storage:20.3.0")
+//    implementation("com.google.firebase:firebase-auth:22.3.1")
+//    implementation("com.google.firebase:firebase-analytics:21.5.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -76,4 +80,8 @@ dependencies {
 
     implementation("io.github.vanpra.compose-material-dialogs:datetime:0.8.1-rc")
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+
+    implementation("io.coil-kt:coil-compose:2.6.0")
+
+    // implementation("com.google.firebase:firebase-crash:16.2.1")
 }
